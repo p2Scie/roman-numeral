96478f3933c0643e56d7af20486f450e49d1fee6
Stevens <sdemervil@gmail.com>
2021-11-03 15:44:45 +0100

    test21

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const letterI = "I";
 const letterV = "V";
 const letterX = "X";
 
+const letters = ["I", "V", "X", "L", "C", "D", "M"];
+
 const decimalToRoman = (num) => {
 
 
@@ -37,24 +39,6 @@ const decimalToRoman = (num) => {
 
 }
 
-console.log(decimalToRoman(0));
-console.log(decimalToRoman(1));
-console.log(decimalToRoman(2));
-console.log(decimalToRoman(3));
-console.log(decimalToRoman(4));
-console.log(decimalToRoman(5));
-console.log(decimalToRoman(6));
-console.log(decimalToRoman(7));
-console.log(decimalToRoman(8));
-console.log(decimalToRoman(9));
-console.log(decimalToRoman(10));
-console.log(decimalToRoman(11));
-console.log(decimalToRoman(12));
-console.log(decimalToRoman(13));
-console.log(decimalToRoman(14));
-console.log(decimalToRoman(15));
-console.log(decimalToRoman(16));
-console.log(decimalToRoman(17));
-console.log(decimalToRoman(18));
-console.log(decimalToRoman(19));
-console.log(decimalToRoman(20));
+for (let i = 1; i <= 3000; i++) {
+    console.log(decimalToRoman(i));
+}
